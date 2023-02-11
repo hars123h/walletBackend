1b0cdf77ee10e27e2cc4895f60da1a5371e489ae
unknown <mprakhar35@gmail.com>
2023-02-11 15:46:54 +0530

    some routes added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require("../model/User");
+const User = require("../model/user");
 const referralCodeGenerator = require('referral-code-generator');
 const axios = require('axios');
 const Plan = require("../model/plan");
